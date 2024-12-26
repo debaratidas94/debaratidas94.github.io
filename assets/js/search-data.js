64e36c05b9acf9dc4125e62eb5dd5fec7e2fd312
debaratidas94 <71130128+debaratidas94@users.noreply.github.com>
2024-12-26 21:05:04 +0000

    Deploying to gh-pages from @ debaratidas94/debaratidas94.github.io@c4a1414eb074c8eb05d820f42b74bac1fdcd7a27 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%65%62%61%72%61%74%69.%64%31%39%39%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://instagram.com/geekylilchef# your instagram id", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/debaratidas1994", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,7 +520,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=NpoVjbMAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
